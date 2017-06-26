680dc9dde84445aa45256d3173f75f2fc62ed55d
ngc92 <ngc92@users.noreply.github.com>
2017-06-26 17:19:27 +0200

    simple testcase

---
+++ tasks/copy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def to_binary(x, l):
+    vec = np.zeros((len(x), l))
+    binary = list(map(lambda x: list(map(float, ("{0:0%ib}"%(l)).format(x))), x))
+    vec[:, :] = binary
+    return vec
+
+class CopyTask:
+    def __init__(self, alphabet, sequence_length):
+        self._alphabet = alphabet
+        self._sequence_length = sequence_length
+
+    def __call__(self, n):
+        # random sequence
+        vec_size = int(np.ceil(np.log(self._alphabet + 1) / np.log(2)))
+        seq = np.random.randint(self._alphabet, size=(n, self._sequence_length)) + 1
+        binaries = np.array(list(map(lambda x: to_binary(x, vec_size), seq)))
+
+        input_seq  = np.zeros((n, self._sequence_length*2+1, vec_size))
+        output_seq = np.zeros((n, self._sequence_length*2+1, vec_size))
+
+        input_seq[:, 0:binaries.shape[1], :] = binaries
+        output_seq[:, binaries.shape[1]:-1, :] = binaries 
+        
+        return input_seq, output_seq
--- test.py
+++ test.py
@@ -3,11 +3,31 @@ import tensorflow as tf
 from dnc import DNC, LSTMCell
 from dnc.memory import NTMMemory
 
+from tasks.copy import CopyTask
+
+INPUT_SIZE = 8
+
 memory = NTMMemory(20, 80)
 rheads = memory.read_heads(5)
 wheads = memory.write_heads(1)
 
-input = tf.placeholder(tf.float32, shape=(10, None, 5))
+input = tf.placeholder(tf.float32, shape=(None, None, INPUT_SIZE))
 lstm  = LSTMCell(25)
 
-net = DNC(input, memory, rheads, wheads, 3, controller = lstm)
+net = DNC(input, memory, rheads, wheads, INPUT_SIZE, controller = lstm)
+targets = tf.placeholder(dtype=tf.float32, shape=[None, None, INPUT_SIZE])
+loss = tf.losses.mean_squared_error(net[0], targets)
+
+opt = tf.train.AdamOptimizer(1e-3)
+train = opt.minimize(loss)
+
+
+task = CopyTask(2**INPUT_SIZE-1, 10)
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+    for i in range(1000):
+        training_set = task(64)
+        l, o = session.run([loss, train], feed_dict={input:training_set[0], targets: training_set[1]})
+        print(l)
+
