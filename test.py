1a03bdf28af552ae67c6de5fd1a891cdba10a829
Philipp Dönges <philipp.doenges@stud.uni-goettingen.de>
2017-07-04 19:17:33 +0200

    scalar summary

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 from dnc import DNC, LSTMCell
 from dnc.memory import Memory, NTMReadHead, NTMWriteHead
 
-from tasks import CopyTask
+from tasks import CopyTask, RepeatCopyTask
 from utils import *
 
 INPUT_SIZE = 8
@@ -23,7 +23,7 @@ targets = tf.placeholder(dtype=tf.float32, shape=[None, None, INPUT_SIZE])
 mask    = tf.placeholder(dtype=tf.float32, shape=[None, None, INPUT_SIZE])
 output  = net[0]
 loss = tf.losses.sigmoid_cross_entropy(logits=output, multi_class_labels=targets)
-cost = tf.reduce_sum((1 - targets * (1 - tf.exp(-output))) * tf.sigmoid(output))
+cost = tf.reduce_sum((1 - targets * (1 - tf.exp(-output))) * tf.sigmoid(output)) / BATCH_SIZE
 
 opt = tf.train.RMSPropOptimizer(1e-4, momentum=0.9)
 train = minimize_and_clip(opt, loss)
@@ -32,7 +32,9 @@ print(net[2])
 names = ["memory", "read", "write"]
 img_summary = [tf.summary.image(name, concate_to_image(data), max_outputs=1) for (name, data) in zip(names, net[2])]
 img_summary = tf.summary.merge(img_summary)
-
+scalar_summary = [tf.summary.scalar("cost", cost), tf.summary.scalar("loss", loss)]
+scalar_summary += [tf.summary.scalar(name, value) for (name, value) in weight_norms()]
+scalar_summary = tf.summary.merge(scalar_summary)
 
 task = CopyTask(2**INPUT_SIZE-1, 10)
 
@@ -47,14 +49,16 @@ with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     for i in range(200*1000):
 
-        lg = 10#np.random.randint(19) + 1
+        lg = np.random.randint(19) + 1
         if i % 100 == 0:
             lg = 20
         training_set = task(BATCH_SIZE, lg)
-        l, o, c, s = session.run([loss, train, cost, img_summary], feed_dict={input:training_set[0], 
-                                                              targets: training_set[1], 
-                                                              mask: training_set[2]})
+        l, o, c, s1, s2 = session.run([loss, train, cost, img_summary, scalar_summary],
+                                        feed_dict={ input:training_set[0],
+                                                    targets: training_set[1], 
+                                                    mask: training_set[2]})
 
+        w.add_summary(s2, global_step=i*BATCH_SIZE)
         if i % 100 == 0:
-            w.add_summary(s, global_step=i)
-        print(i * BATCH_SIZE // 1000, l, c/BATCH_SIZE)
+            w.add_summary(s1, global_step=i*BATCH_SIZE)
+        print(i * BATCH_SIZE / 1000, l, c)
